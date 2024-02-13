807e8cdc7e699eeba575d0f01c022632767c8bab
mirajsankdecha <mirajsankdecha@gmail.com>
2024-02-13 23:35:17 +0530

    Shutdown Tool Created

---
+++ Shutdown Tool.bat
@@ -0,0 +1,46 @@
+@echo off
+chcp 65001 > nul
+
+echo.
+echo.	 
+echo     ╔═════════════════════════════════════════╗
+echo     ║                                         ║
+echo     ║          Miraj Sankdecha                ║
+echo     ║                                         ║
+echo     ╚═════════════════════════════════════════╝
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo.
+    echo Error: This script requires administrator privileges. Please run it as an administrator.
+    pause
+    exit /b
+)
+
+echo.
+echo Your PC's IP address:
+ipconfig | findstr IPv4
+echo.
+echo List of IP addresses connected to the network:
+arp -a
+echo.
+
+set /p ip=Enter the IP address of the Shutdown computer: 
+
+if "%ip%"=="" (
+    echo.
+    echo Error: IP address not provided.
+    pause
+    exit /b
+)
+
+shutdown /s /m \\%ip% /t 0 /f
+if %errorlevel% equ 0 (
+    echo.
+    echo Shutdown command sent successfully.
+) else (
+    echo.
+    echo Error: Shutdown command failed.
+)
+
+pause

29de8336d71ae2fa2e92849fe4a8ef5ccd75a8ca
mirajsankdecha <mirajsankdecha@gmail.com>
2024-02-13 23:53:34 +0530

    Portscan tool update

--- Portscan.py
+++ Portscan.py
@@ -15,14 +15,25 @@ def scan_ports(ip, ports):
         sock.close()
     return open_ports
 
+def get_port_range():
+    while True:
+        port_range = input("Enter port range (e.g., 1-100): ")
+        if '-' in port_range:
+            try:
+                start_port, end_port = map(int, port_range.split('-'))
+                return start_port, end_port
+            except ValueError:
+                print(colored("Invalid port range format. Please try again.", "red"))
+        else:
+            print(colored("Port range should be in the format of 'start_port-end_port'. Please try again.", "red"))
+
 if __name__ == "__main__":
     print(colored("Port Scanner Tool", "blue"))
     print(colored("This tool is for educational purposes and created by Miraj Sankdecha.", "blue"))
     print(colored("=" * 50, "blue"))
 
     target_ips = input("Enter target IP addresses separated by comma: ").split(',')
-    port_range = input("Enter port range (e.g., 1-100): ")
-    start_port, end_port = map(int, port_range.split('-'))
+    start_port, end_port = get_port_range()
     ports = range(start_port, end_port + 1)
     
     for ip in target_ips:
